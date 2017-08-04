ce5bb26c338ead583d85bd2648329f3e48d27013
a523010157 <xubo-mugua@qq.com>
2017-08-04 14:34:45 +0800

    My First Commit!

---
+++ src/main/java/com/xubo/frameexample/FrameexampleApplication.java
@@ -0,0 +1,12 @@
+package com.xubo.frameexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameexampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xubo/frameexample/FrameexampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xubo.frameexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrameexampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
